fd5892de50f3b5ccf6eed9ba2e7339f154ba15c4
mrparalon <mrparalon@mr.paralon-Kubuntu>
2019-10-01 21:34:15 +0300

    Tthottling added

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -160,4 +160,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
 
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+
 }
